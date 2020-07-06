2eb23c3bd0e8bc410114db6a858def4d58101558
arikankainen <60932647+arikankainen@users.noreply.github.com>
2020-07-06 11:34:26 +0300

    apollo asennettu, testiquery backendiin toimii

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000'
+  })
+});
+
+const query = gql`
+  query {
+    listUsers {
+      username,
+      password,
+      email,
+      fullname,
+      isAdmin,
+      id
+    }
+  }
+`;
+
+client
+  .query({ query })
+  .then(response => {
+    console.log(response.data);
+  });
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
