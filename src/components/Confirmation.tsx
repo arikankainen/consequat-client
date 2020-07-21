73f27d3c3abd4d90a33132e3dd358732054133cc
arikankainen <60932647+arikankainen@users.noreply.github.com>
2020-07-22 01:06:28 +0300

    luotu alustava createPortalilla tehty confirmation-modal

---
+++ src/components/Confirmation.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Modal from './Modal';
+import styled from 'styled-components';
+
+const BackDrop = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 1002;
+`;
+
+const Container = styled.div`
+  position: fixed;
+  width: 300px;
+  height: 150px;
+  top: 200px;
+  left: 200px;
+  background-color: #777;
+  z-index: 1002;
+`;
+
+const Confirmation = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <Modal isOpen={isOpen}>
+      <BackDrop>
+        <Container>
+          Jeps
+          <button onClick={() => setIsOpen(false)}>Close</button>
+        </Container>
+      </BackDrop>
+    </Modal>
+  );
+};
+
+export default Confirmation;
