2ae46e57b7e302639dc2f2bd39503fa0f64b6b7b
arikankainen <60932647+arikankainen@users.noreply.github.com>
2020-09-29 08:44:03 +0300

    korvattu useampi taustakuvakoko yhdellä

--- src/pages/App/style.ts
+++ src/pages/App/style.ts
@@ -1,9 +1,5 @@
 import styled, { css } from 'styled-components/macro';
-
-import back1280 from '../../images/back_01_1280.jpg';
 import back1920 from '../../images/back_01_1920.jpg';
-import back2560 from '../../images/back_01_2560.jpg';
-import back3840 from '../../images/back_01_3840.jpg';
 
 interface SiteContainerProps {
   picture: boolean;
@@ -27,22 +23,7 @@ export const SiteContainer = styled.section<SiteContainerProps>`
       background-position: center center;
       background-repeat: no-repeat;
       background-size: cover;
-      
-      @media screen and (max-width: 1280px) {
-        background-image: url(${back1280});
-      }
-
-      @media screen and (min-width: 1281px) and (max-width: 1920px) {
-        background-image: url(${back1920});
-      }
-
-      @media screen and (min-width: 1921px) and (max-width: 2560px) {
-        background-image: url(${back2560});
-      }
-
-      @media screen and (min-width: 2561px) {
-        background-image: url(${back3840});
-      }
+      background-image: url(${back1920});
   `}
 `;
 
